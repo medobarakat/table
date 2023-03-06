dbf5a1075740a8c245fafa928bedb1b003db07f5
medobarakat <ahmedbarakat2401@gmail.com>
2023-03-06 03:44:31 -0800

    finish task 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const data = {
+    "items":{
+      "item":{
+        "id":"001",
+        "battters":{
+          "batter":[
+            {
+              "id":"1001",
+              "type":"regular"
+            },
+            {
+              "id":"1002",
+              "type":"unreg3ular"
+            },
+            {
+              "id":"1003",
+              "type":"unre4gular"
+            },
+            {
+              "id":"1004",
+              "type":"unr2egular"
+            },
+            {
+              "id":"1005",
+              "type":"un1regular"
+            }
+          ]
+        },
+        "otherdata":"otherdata"
+      }
+    }
+  }
+
+
+  const DisplayData=data.items.item.battters.batter.map(
+    (info)=>{
+        return(
+            <tr>
+                <td>{info.id}</td>
+                <td>{info.type}</td>
+            </tr>
+        )
+    }
+)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+            <th>id</th>
+            <th>type</th>
+            </tr>
+        </thead>
+        <tbody>
+         
+            
+          {DisplayData}
+            
+        </tbody>
+    </table>
+     
+</div>
   );
 }
 
